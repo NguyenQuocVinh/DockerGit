a703e85fc842f63e0d4559f0de3d3bdb532a451b
NguyenQuocVinh <quocvinh.n@gmail.com>
2022-04-07 10:19:09 +0700

    Update step3.sh

--- step3.sh
+++ step3.sh
@@ -1,52 +1,33 @@
 #Create instance db01
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo 'db01:/u01/app/oracle/product/11.2.0.4/db_1/:N' >> /etc/oratab"
-docker exec -d -u oracle ol7-db11r2 /bin/bash -c "cp /u01/Docker/install/initdb01.ora /u01/app/oracle/product/11.2.0.4/db_1/dbs/"
+_container='ol7-db11r2'
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'db01:/u01/app/oracle/product/11.2.0.4/db_1/:N' >> /etc/oratab"
+docker exec -d -u oracle $_container /bin/bash -c "cp /u01/Docker/install/initdb01.ora /u01/app/oracle/product/11.2.0.4/db_1/dbs/"
 
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo 'export ORAENV_ASK=NO' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo '. .oraenv' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo 'sqlplus / as sysdba <<EOF' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo 'create spfile from pfile;' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo 'startup nomount' >> /tmp/create_instance.txt"
-_x='CREATE DATABASE "db01"'
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo $_x >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo 'USER SYS IDENTIFIED BY abc123 USER SYSTEM IDENTIFIED BY abc123' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo 'EXTENT MANAGEMENT LOCAL' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo 'DEFAULT TEMPORARY TABLESPACE temp UNDO TABLESPACE undotbs1 DEFAULT TABLESPACE users datafile size 500M;' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo '/' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo '@?/rdbms/admin/catalog.sql' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo '@?/rdbms/admin/catproc.sql' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo '@?/rdbms/admin/utlrp.sql' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo 'exit' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo 'EOF' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'export ORAENV_ASK=NO' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo '. .oraenv' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'sqlplus / as sysdba <<EOF' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'create spfile from pfile;' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'startup nomount' >> /tmp/create_instance.txt"
+_x='CREATE DATABASE \"db01\"'
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo $_x >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'USER SYS IDENTIFIED BY abc123 USER SYSTEM IDENTIFIED BY abc123' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'EXTENT MANAGEMENT LOCAL' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'DEFAULT TEMPORARY TABLESPACE temp UNDO TABLESPACE undotbs1 DEFAULT TABLESPACE users datafile size 500M;' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo '/' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo '@?/rdbms/admin/catalog.sql' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo '@?/rdbms/admin/catproc.sql' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo '@?/rdbms/admin/utlrp.sql' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'exit' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'EOF' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "sh /tmp/create_instance.txt"
 
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo 'connect system/abc123' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo '@?/sqlplus/admin/pupbld.sql' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo '' >> /tmp/create_instance.txt"
-docker exec -d -u oracle -w /home/oracle ol7-db11r2 /bin/bash -c "echo '' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'export ORAENV_ASK=NO' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo '. .oraenv' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'sqlplus system/abc123 <<EOF' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo '@?/sqlplus/admin/pupbld.sql' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'EOF' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "echo 'exit' >> /tmp/create_instance.txt"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "sh /tmp/create_instance.txt"
 
-
-# tam thoi dung o day, se dua may cai nay vo scrip luon
-docker exec -u oracle -w /home/oracle -it ol7-db11r2 /bin/bash
-. .bash_profile
-. oraenv			--> choose sid= db01 and enter
-sqlplus / as sysdba
-SQL>create spfile from pfile;
-SQL>startup nomount
-SQL> CREATE DATABASE "db01"
-USER SYS IDENTIFIED BY abc123 USER SYSTEM IDENTIFIED BY abc123
-EXTENT MANAGEMENT LOCAL 
-DEFAULT TEMPORARY TABLESPACE temp UNDO TABLESPACE undotbs1 DEFAULT TABLESPACE users datafile size 500M;
-/
-@?/rdbms/admin/catalog.sql
-@?/rdbms/admin/catproc.sql
-@?/rdbms/admin/utlrp.sql
-
-sqlplus	system/abc123
-SQL> @?/sqlplus/admin/pupbld.sql
-
-#create password file
-$oh
-$orapwd file=orapwdb01 password=ABC entries=5 force=y
-
-#start listener
-$lsnrctl start
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "export ORAENV_ASK=NO && . .oraenv && orapwd file=orapwdb01 password=ABC entries=5 force=y"
+docker exec -d -u oracle -w /home/oracle $_container /bin/bash -c "export ORAENV_ASK=NO && . .oraenv && lsnrctl start"
